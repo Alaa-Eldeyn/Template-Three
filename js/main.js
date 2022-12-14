a05cb30c532da033a4cac5ec208918886451ae13
Alaa Eldeyn <93477209+Alaa-Eldeyn@users.noreply.github.com>
2022-12-15 01:11:34 +0200

    adding more features
    -- progress bars
    -- stats count

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Date counter
 let countDownDate = new Date("Jul 15, 2023 00:00:00").getTime()
 let counter = setInterval(()=>{
     let dateNow = new Date().getTime();
@@ -13,3 +14,40 @@ let counter = setInterval(()=>{
     time[3].textContent = seconds < 10 ? `0${seconds}` : seconds;
     if (dateDiff = 0) clearInterval(counter);
 }, 1000);
+
+
+// Progress
+let progressSection = document.getElementById("skills");
+let progressSpans = document.querySelectorAll(".skills .progress div span");
+
+// Stats Counter
+let statsSection = document.getElementById("stats")
+let statsNums = document.querySelectorAll(".stats .nums .box h4");
+let started = false;
+
+window.onscroll = function () {
+    // Progress
+    if (window.scrollY >= progressSection.offsetTop - 200) {
+        progressSpans.forEach((span) => {
+            span.style.width = span.dataset.width ;
+        })
+    }
+    // Stats Counter
+    if (window.scrollY >= statsSection.offsetTop - 200) {
+        if (!started) {
+            statsNums.forEach((num) => countUp(num));
+        }
+        started = true
+    }
+}
+
+
+function countUp(el) {
+    let goal = el.dataset.goal;
+    let counter = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(counter);
+        }
+    }, 2000 / goal);
+}
